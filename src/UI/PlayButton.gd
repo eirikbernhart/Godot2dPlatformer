35e8186134540aa26920b362d887e8c4e3189f3b
bernieboy <bereir15@student.westerdals.no>
2020-08-19 18:02:02 +0200

    Logic to pause overlay

--- src/UI/PlayButton.gd
+++ src/UI/PlayButton.gd
@@ -4,6 +4,7 @@ extends Button
 export(String, FILE) var next_scene_path: = ""
 
 func _on_button_up() -> void:
+    get_tree().paused = false
     get_tree().change_scene(next_scene_path)
 
 func _get_configuration_warning() -> String:
--- src/UI/UserInterface.gd
+++ src/UI/UserInterface.gd
@@ -5,6 +5,8 @@ onready var pause_overlay: ColorRect = get_node("PauseOverlay")
 onready var score: Label = get_node("Label")
 onready var paused_title: Label = get_node("PauseOverlay/Title")
 
+const DIED_MESSAGE: = "Youd died!"
+
 var paused: = false setget set_paused
 
 func _ready() -> void:
@@ -15,10 +17,10 @@ func _ready() -> void:
 
 func _on_PlayerData_player_died() -> void:
     self.paused = true
-    paused_title.text = "You died!"
+    paused_title.text = DIED_MESSAGE
     
 func _unhandled_input(event: InputEvent) -> void:
-    if event.is_action_pressed("pause"):
+    if event.is_action_pressed("pause") and paused_title.text != DIED_MESSAGE:
         self.paused = not paused
         scene_tree.set_input_as_handled() # Only this func will process the esc-event 
 
