e096a4f0bb1d4234e7e4f35c56ff86c19cce9ed2
Eirik Bernhart <eirik@beiningbogen.no>
2020-09-03 17:58:59 +0200

    Mobile controls WIP

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,10 +1,10 @@
 extends Actor
 
 enum STATES {
-    MoveRight,
-    MoveLeft,
-    Jump
-    Idle
+	MoveRight,
+	MoveLeft,
+	Jump
+	Idle
 }
 
 onready var state = STATES.Idle
@@ -12,48 +12,53 @@ onready var state = STATES.Idle
 export var stomp_impulse = 1000.0
 
 func _on_EnemyDetector_area_entered(area: Area2D) -> void:
-    _velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
-    
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+	
 func _on_EnemyDetector_body_entered(body: Node) -> void:
-    die()
-    
+	die()
+	
 func _physics_process(delta: float) -> void:
-    var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 #    var direction = get_direction()
 #    _velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 #    _velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-     
-    _velocity.y += gravity * get_physics_process_delta_time()
+	 
+	_velocity.y += gravity * get_physics_process_delta_time()
 
-    match state: 
-        STATES.MoveRight:
-            print("move right")
-            _velocity.x = min(_velocity.x + 300, 500)
-        STATES.MoveLeft:
-            print("move left")
-            _velocity.x = max(_velocity.x - 300, -500)
-#        STATES.Jump:
-#            print("jump")
-        STATES.Idle:
-            _velocity.x = 0
-            
-    move_and_slide(_velocity, FLOOR_NORMAL)
-    
+	match state: 
+		STATES.MoveRight:
+			print("move right")
+			_velocity.x = min(_velocity.x + 300, 500)
+		STATES.MoveLeft:
+			print("move left")
+			_velocity.x = max(_velocity.x - 300, -500)
+		STATES.Jump:
+			print("jump")
+		STATES.Idle:
+			_velocity.x = 0
+			print("idle")
+			
+	move_and_slide(_velocity, FLOOR_NORMAL)
+	
 func _input(event):
-    if event is InputEventScreenTouch:
-        if event.pressed:
-            var local_event = make_input_local(event)
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			var local_event = make_input_local(event)
 
-            if local_event.position.x > 100:
-                state = STATES.MoveRight
-            elif local_event.position.x < -100:
-                state = STATES.MoveLeft
-#            elif local_event.position.y in range(-100, -50):
-#                state = STATES.Jump
-    else:
-        state = STATES.Idle
-    
-    
+			if local_event.position.x > 100:
+				print("TOUCHED RIGHT")
+				state = STATES.MoveRight
+			elif local_event.position.x < -100:
+				print("TOUCHED LEFT")
+				state = STATES.MoveLeft
+			elif local_event.position.y in range(-100, -50):
+				print("TOUCHED PLAYER")
+				state = STATES.Jump
+	else:
+		state = STATES.Idle # Probably triggered way too fast...
+#		print("NO FCKN IDEA...")
+	
+	
 #func get_direction() -> Vector2:
 #    return Vector2(
 #            Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
@@ -79,16 +84,16 @@ func _input(event):
 #        return out
 
 func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
-    var out: = linear_velocity
-    out.y = -impulse
-    return out
-    
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
+	
 func die() -> void:
-    PlayerData.deaths += 1
-    queue_free()
-    
-    
-    
+	PlayerData.deaths += 1
+	queue_free()
+	
+	
+	
 
 
 
--- src/Autoload/PlayerData.gd
+++ src/Autoload/PlayerData.gd
@@ -7,13 +7,13 @@ var score : = 0 setget set_score
 var deaths: = 0 setget set_deaths
 
 func reset() -> void:
-    score = 0
-    deaths = 0
+	score = 0
+	deaths = 0
 
 func set_score(value: int) -> void:
-    score = value
-    emit_signal("score_updated")
+	score = value
+	emit_signal("score_updated")
 
 func set_deaths(value: int) -> void:
-    deaths = value
-    emit_signal("player_died")
+	deaths = value
+	emit_signal("player_died")
